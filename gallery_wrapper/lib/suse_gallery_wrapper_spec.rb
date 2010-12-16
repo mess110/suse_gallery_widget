b5ee3b5847f8294ac4b68c824612b90a9c580341
Ionuț Arțăriși <iartarisi@suse.de>
2010-12-16 18:03:30 +0100

    test refresh and raising errors

--- gallery_wrapper/lib/suse_gallery_wrapper_spec.rb
+++ gallery_wrapper/lib/suse_gallery_wrapper_spec.rb
@@ -14,16 +14,34 @@ describe SuseGalleryWrapper do
 
   before :all do
     FakeWeb.register_uri(:get,
-                         "http://#{BASIC_AUTH_USER}:#{BASIC_AUTH_PASS}@#{BASE_URL}/api/v2/gallery/appliances_list/?popular=''&per_page=5",
+                         /http:\/\/#{BASIC_AUTH_USER}:#{BASIC_AUTH_PASS}@#{BASE_URL}\/api\/v2\/gallery\/appliances_list.*/,
                          :body => File.read("%s/popular_5.xml" % FIXTURES))
     @gallery = SuseGalleryWrapper.new
   end
 
   describe "#get_logo" do
     it "returns an the location to the appliance id" do
-      GalleryRequest.stub(:system) {:foo}
+      GalleryRequest.stub(:system)
 
       @gallery.get_logo(3).should == "/tmp/3"
+    end
+  end
+
+  describe "#appliances" do
+    it "length should be 5" do
+      @gallery.appliances.length.should == 5
+    end
+  end
+
+  describe "#refresh" do
+    it "raises InvalidRequestType if given a bad appliance type" do
+      lambda{ @gallery.refresh("badtype", 5) }.should raise_error(InvalidRequestType)
+    end
+  end
+
+  describe "#start_testdrive" do
+    it "raises InvalidApplianceId if given a bad appliance_id" do
+      lambda { @gallery.start_testdrive "0" }.should raise_error(InvalidApplianceId)
     end
   end
 end
