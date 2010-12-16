b2b24963950ffa6209d8b00352ce83ea82854c28
Flavio Castelli <fcastelli@novell.com>
2010-12-16 11:06:59 +0100

    Star popular appliances from KDE plasmoid

--- kde/contents/code/main.rb
+++ kde/contents/code/main.rb
@@ -15,24 +15,27 @@ module SuseGalleryPlasmoid
       self.has_configuration_interface = false
       self.aspect_ratio_mode = Plasma::Square
       self.background_hints = Plasma::Applet.DefaultBackground
- 
+      
       layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
-      @gallery.appliances.each do |key, value|
-        line = Qt::GraphicsLinearLayout.new(Qt::Horizontal, layout) do
-          label = Plasma::Label.new
-          label.text = value
+      @gallery.appliances.each do |appliance_id, appliance_name|
+        line_layout = Qt::GraphicsLinearLayout.new(Qt::Horizontal, layout)
+        label = Plasma::Label.new
+        label.text = appliance_name
           
-          button = Plasma::PushButton.new
-          button.text = "Push me!"
-          button.image = @testdrive_icon
-          button.connect(SIGNAL(:clicked)) do
-            puts "button #{key} has been pressed"
-          end
-
-          add_item label
-          add_item button
+        button = Plasma::PushButton.new
+        #button.text = "Push me!"
+        button.setStyleSheet("width: 15px")
+        button.image = @testdrive_icon
+        button.setMinimumSize 24,24
+        button.setMaximumSize 24,24
+        button.connect(SIGNAL(:clicked)) do
+          @gallery.start_testdrive appliance_id
         end
-        layout.add_item line
+
+        line_layout.add_item label
+        line_layout.add_item button
+        
+        layout.add_item line_layout
       end
       self.layout = layout
     end
