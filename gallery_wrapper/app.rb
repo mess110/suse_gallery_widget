bc12ab971a76dc26f897c6792c05fa020ac9f163
Cristian Mircea Messel <cmessel@suse.de>
2010-12-16 15:59:57 +0100

    show more debugging info

--- gallery_wrapper/app.rb
+++ gallery_wrapper/app.rb
@@ -2,5 +2,6 @@ require 'rubygems'
 require 'suse_gallery_wrapper'
 
 wrapper = SuseGalleryWrapper.new
+puts wrapper.appliances.size
 puts wrapper.get_logo(238737)
 puts wrapper.start_testdrive(wrapper.appliances.keys[0])
