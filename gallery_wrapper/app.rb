03f050de0a2b91a89484ea2eeac0c32065d2ae7d
Flavio Castelli <fcastelli@novell.com>
2010-12-16 17:00:46 +0100

    Added more logging to app.rb

--- gallery_wrapper/app.rb
+++ gallery_wrapper/app.rb
@@ -1,7 +1,13 @@
 require 'rubygems'
+require 'logger'
 require 'suse_gallery_wrapper'
 
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
+
+log.debug "contacting suse studio gallery"
 wrapper = SuseGalleryWrapper.new
-puts wrapper.appliances.size
-puts wrapper.get_logo(238737)
-puts wrapper.start_testdrive(wrapper.appliances.keys[0])
+log.debug "Number of popular appliances: #{wrapper.appliances.size}"
+log.debug "Appliance logo: #{wrapper.get_logo(238737)}"
+log.debug "Starting a public testdrive"
+log.debug "Public testdrive response: #{wrapper.start_testdrive(wrapper.appliances.keys[0])}"
