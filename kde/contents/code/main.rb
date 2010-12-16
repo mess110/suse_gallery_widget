2c12143708b12ef056fa0e385315a4de5a76407b
Flavio Castelli <fcastelli@novell.com>
2010-12-16 18:43:20 +0100

    more work on kde plasmoid

--- kde/contents/code/main.rb
+++ kde/contents/code/main.rb
@@ -1,5 +1,6 @@
 # <Copyright and license information goes here.>
 require 'rubygems'
+require 'korundum4'
 require 'plasma_applet'
 require 'suse_gallery_wrapper'
  
@@ -8,36 +9,68 @@ module SuseGalleryPlasmoid
     def initialize parent
       super parent
       @testdrive_icon = package.filePath("images", "action-testdrive.png")
-      @gallery = SuseGalleryWrapper.new
+      Thread.abort_on_exception = true
     end
  
     def init
       self.has_configuration_interface = false
       self.aspect_ratio_mode = Plasma::Square
       self.background_hints = Plasma::Applet.DefaultBackground
-      
-      layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
-      @gallery.appliances.each do |appliance_id, appliance_name|
-        line_layout = Qt::GraphicsLinearLayout.new(Qt::Horizontal, layout)
-        label = Plasma::Label.new
-        label.text = appliance_name
-          
-        button = Plasma::PushButton.new
-        #button.text = "Push me!"
-        button.setStyleSheet("width: 15px")
-        button.image = @testdrive_icon
-        button.setMinimumSize 24,24
-        button.setMaximumSize 24,24
-        button.connect(SIGNAL(:clicked)) do
-          @gallery.start_testdrive appliance_id
-        end
 
-        line_layout.add_item label
-        line_layout.add_item button
-        
-        layout.add_item line_layout
-      end
+      @layout = Qt::GraphicsLinearLayout.new Qt::Vertical, self
+
+      @busy_widget = Plasma::BusyWidget.new
+      @busy_widget.label = "Fetching popular appliances"
+      @layout.add_item @busy_widget
+      
       self.layout = layout
+
+      refresh
     end
+
+    private
+
+    def refresh
+      #Thread.new {
+        puts "starting to fetch"
+        #@layout.add_item @busy_widget
+        
+        @gallery = SuseGalleryWrapper.new
+        #@layout.remove_item @busy_widget
+        puts "fetch done"
+        @busy_widget.set_visible false
+        
+        @gallery.appliances.each do |appliance_id, appliance_name|
+          line_layout = Qt::GraphicsLinearLayout.new(Qt::Horizontal, layout)
+          label = Plasma::Label.new
+          label.text = appliance_name
+          
+          button = Plasma::PushButton.new
+          #button.text = "Push me!"
+          button.image = @testdrive_icon
+          button.setMinimumSize 24,24
+          button.setMaximumSize 24,24
+          button.connect(SIGNAL(:clicked)) do
+            Thread.new {
+              begin
+                raise "foo"
+                puts "starting testdrive #{appliance_id}"
+                td = @gallery.start_testdrive(appliance_id)
+                puts td.inspect
+                @gallery.connect_to_testdrive(td)
+              rescue
+                KDE::MessageBox.error(self, "#{$!}", "SUSE gallery plasmoid - error")
+              end
+            }
+          end
+
+          line_layout.add_item label
+          line_layout.add_item button
+          
+          @layout.add_item line_layout
+        end
+      #}
+   end
+
   end
 end
